24069e25bc5d6a4e8a507e99076a62fed553d6bf
Eduardo Rosas | Applied Math & Data Analysis <rosas.eduardovazquez@gmail.com>
2026-01-14 02:32:12 -0600

    Update 03_boletos_cine.c

--- 03_boletos_cine.c
+++ 03_boletos_cine.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+// funcion presentacion
+void caratula() 
+{
+    printf("Venta De Boletos De Cine\n");
+    printf("Alumno: Eduardo Rosas Vazquez\n");
+    printf("Materia: Programacion III - UNAM\n\n");
+    
+    printf("\nPresiona ENTER para comenzar el programa...");
+    getchar();
+}
+
+int main()
+{
+  caratula();
+}
