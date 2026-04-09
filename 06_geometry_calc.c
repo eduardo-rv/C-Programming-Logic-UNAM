c21d5a8ef61cf51306353a5ca3fb131b5c1a641e
Eduardo Rosas <rosas.eduardovazquez@gmail.com>
2026-04-09 15:34:58 -0600

    complete

--- 06_geometry_calc.c
+++ 06_geometry_calc.c
@@ -1,35 +1,38 @@
-// mediante un menu de opciones, calcular el area y el perimetro de las siguientes figuras geometricas
-// circulo, rectangulo, triangulo, rombo, trapecio. usar do while para seguir ejecutando hasta que el usuario quiera
-
 #include <stdio.h>
 #include <stdlib.h>
+#define PI 3.14159
 
 void ClearScreen();
 void Display();
 int Options();
-void Calculator(int option);
+void Calculator(int choice);
 
 
 int main()
 {
-	int option;
+	int choice;
 
 	Display();
 
 
 	do
 	{
-		option = Options();
-		Calculator(option);
+		choice = Options();
+		if(choice != 6)
+		{
+			Calculator(choice);
+		}
 
-	}while(option < 6);
-
+	}while(choice < 6);
+	
+	printf("\nExiting program.\n\n");
 	return 0;
 }
 
 void ClearScreen()
 {
 	printf("\n\npress ENTER to continue...");
+	while(getchar() != '\n');
 	getchar();
 	system("clear");
 }
@@ -37,15 +40,18 @@ void ClearScreen()
 void Display()
 {
 	printf("Geometry calculator\n");
-	printf("This program is a Geometry Calculator. It allows you to select a geometric shape from a menu to calculate its area and perimeter.\n\n");
-	printf("Author: Rosas Vazquez Eduardo");
+	printf("Description: This program is a Geometry Calculator. It allows you to select a geometric shape from a menu to calculate its area and perimeter.\n\n");
+	printf("Author: Rosas Vazquez Eduardo\n\n");
 
-	ClearScreen();
+	printf("press ENTER to continue...");
+	getchar();
+	system("clear");
+
 }	
 
 int Options()
 {
-	int option;
+	int choice;
 
 	printf("1. Circle\n");
 	printf("2. Rectangle\n");
@@ -53,34 +59,33 @@ int Options()
 	printf("4. Rhombus\n");
 	printf("5. Trapezoid\n");
 	printf("6. Exit\n");
-	printf("Choose an option: ");
-	scanf("%d", &option);
+	printf("Select an option: ");
+	scanf("%d", &choice);
 	
-	return option;
+	return choice;
 }
 
 void Calculator(int option)
 {
-	float major_diagonal, side, minor_diagonal, area, perimeter, radio, pi = 3.1416, base, height;  
+	float major_base, minor_base, major_diagonal, side1, side2, minor_diagonal, area, perimeter, radius, base, height;  
 
 	switch(option)
 	{
 		case 1:
-			printf("\n\nEnter the value for radio: ");
-			scanf("%f", &radio);
+			printf("\n\nEnter the radius: ");
+			scanf("%f", &radius);
 	
-			area = pi * radio * radio;
-			perimeter = 2 * pi * radio;
+			area = PI * radius * radius;
+			perimeter = 2 * PI * radius;
 
 			printf("\n\nArea = %.2f", area);
 			printf("\nPerimeter = %.2f\n\n", perimeter);
 			
-			//pause
 			break;
 		case 2:
-			printf("\n\nEnter the value for base: ");
+			printf("\n\nEnter base: ");
 			scanf("%f", &base);
-			printf("\n\nEnter the value for height: ");
+			printf("Enter height: ");
 			scanf("%f", &height);
 
 			area = base * height;
@@ -89,42 +94,59 @@ void Calculator(int option)
 			printf("\n\nArea = %.2f", area);
 			printf("\nPerimeter = %.2f", perimeter);
 
-			//pause
 			break;
 		case 3:
-			printf("\n\nEnter the value for base: ");
+			printf("\n\nEnter base: ");
 			scanf("%f", &base);
-			printf("\n\nEnter the value for height: ");
+			printf("Enter height: ");
 			scanf("%f", &height);
+			printf("Enter side 1: ");
+			scanf("%f", &side1);
+			printf("Enter side 2: ");
+			scanf("%f", &side2);
 
 			area = (base * height)/2;
-			perimeter = (base + height) * 2;
+			perimeter = (base + side1 + side2);
 
 			printf("\n\nArea = %.2f", area);
 			printf("\nPerimeter = %.2f", perimeter);
 
-			//pause
 			break;
 		case 4:
-			printf("\n\nEnter the value for major diagonal: ");
+			printf("\n\nEnter major diagonal: ");
 			scanf("%f", &major_diagonal);
-			printf("\n\nEnter the value for minor diagonal: ");
+			printf("Enter minor diagonal: ");
 			scanf("%f", &minor_diagonal);
-			printf("Enter the value for side");
-			scanf("%f", &side);
+			printf("Enter side: ");
+			scanf("%f", &side1);
 
 			area = (major_diagonal * minor_diagonal)/2;
-			perimeter = side * 4;
+			perimeter = side1 * 4;
 
 			printf("\n\nArea = %.2f", area);
 			printf("\nPerimeter = %.2f", perimeter);
 
-			//pause
 			break;
 		case 5:
-			printf("Trapezoid");
+			printf("\nEnter major base: ");
+			scanf("%f", &major_base);
+			printf("Enter minor base: ");
+			scanf("%f", &minor_base);
+			printf("Enter height: ");
+			scanf("%f", &height);
+			printf("Enter the two non-parallel sides: ");
+			scanf("%f %f", &side1, &side2);
+
+			area = ((major_base + minor_base) * height) / 2;
+			perimeter = major_base + minor_base + side1 + side2;
+
+			printf("\n\nArea = %.2f", area);
+			printf("\nPerimeter = %.2f", perimeter);
+
 			break;
 		case 6:
 			break;
 	}
+
+	ClearScreen();
 }
