59d01fa4c080656f46282053dea1cd9404f6bf89
Eduardo Rosas <rosas.eduardovazquez@gmail.com>
2026-04-08 19:27:01 -0600

    complete

--- 05_classification_triangles.c
+++ 05_classification_triangles.c
@@ -1,20 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void Clear_Screen();
+void Clear_screen();
 void Display_header();
-
+int Validate_triangle(float a, float b, float c);
+void Classify_triangle(float a, float b, float c);
 
 int main()
 {
+	float side_a, side_b, side_c;
+	int triangle;
+
 	Display_header();
-	printf("Hello world");
+	
+	printf("Enter the value for side A: ");
+	scanf("%f", &side_a);
+	printf("Enter the value for side B: ");
+	scanf("%f", &side_b);
+	printf("Enter the value for side C: ");
+	scanf("%f", &side_c);
+	
+	triangle = Validate_triangle(side_a, side_b, side_c);
+
+	if (triangle == 1)
+	{
+		Classify_triangle(side_a, side_b, side_c);
+	}
+	else
+	{
+		printf("\n\nIt is not a triangle\n\n");
+	}
+
 
 	return 0;
 }
 
-void Clear_Screen()
+void Clear_screen()
 {
+	printf("\n\npress to ENTER to continue...");
 	getchar();
 	system("clear");
 }
@@ -23,8 +46,39 @@ void Display_header()
 {
 	printf("\nCLASSIFICATION OF TRIANGLES\n");
 	printf("This program asks you for the lengths of three sides and determines if the triangle is equilateral, isosceles, or scalene. \n\n");
-	printf("Rosas Vazquez Eduardo\n\n");
-	printf("press to ENTER to continue...");
+	printf("Rosas Vazquez Eduardo");
 
-	Clear_Screen();
+	Clear_screen();
 }
+
+int Validate_triangle(float a, float b, float c)
+{
+	
+	if(((a+b)>c) && ((a+c)>b) && ((b+c)> a))
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+void Classify_triangle(float a, float b, float c)
+{
+	if(a == b && b == c)
+	{
+		printf("\n\nEquilateral triangle\n\n");
+	}
+	else if(a == b || b == c || a == c)
+	{
+		printf("\n\nIsosceles triangle\n\n");
+	}
+	else
+	{
+		printf("\n\nEcalene triangle\n\n");
+	}
+
+}
+
+
