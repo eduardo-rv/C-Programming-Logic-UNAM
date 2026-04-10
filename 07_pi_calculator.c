56b185e35023d64cbc34c6417d84933d05121614
Eduardo Rosas <rosas.eduardovazquez@gmail.com>
2026-04-10 10:32:57 -0600

    update

--- 07_pi_calculator.c
+++ 07_pi_calculator.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void ClearScreen();
+void Display();
+
 int main()
 {
-	printf("Hellow World");
+	Display();
 
 	return 0;
+}
+
+void ClearScreen()
+{
+	printf("\n\npress ENTER to continue...");
+	while(getchar() != '\n');
+	getchar();
+	system("clear");
+}
+
+void Display()
+{
+	printf("PI calculator.\n");
+	printf("Description: This tool calculates PI by summing a series of fractions. The precision increases as more terms are added to the calculation.\n\n");
+
+	printf("Author: Rosas Vazquez Eduardo.");
+
+	printf("press ENTER to continue...");
+	getchar();
+	system("clear");
 }
